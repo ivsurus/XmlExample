eb78a6f99a1aa070595a4c631e08940416abb611
ivan_surus <Ivan@sklbook.mks.local>
2017-02-21 00:06:50 +0300

    add dom

--- src/task5/sax/dom/DOMMenuParser.java
+++ src/task5/sax/dom/DOMMenuParser.java
@@ -8,12 +8,11 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import task5.sax.bean.Appetizer;
 import task5.sax.bean.Food;
+import task5.sax.bean.menuName.MenuAttributeName;
+import task5.sax.bean.menuName.MenuTagName;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class DOMMenuParser {
@@ -24,26 +23,44 @@ public class DOMMenuParser {
         List<Food> menu = new ArrayList<Food>();
         //создание DOM-анализатора (Xerces)
         DOMParser parser = new DOMParser();
-        parser.parse("menu2.xml");
+        parser.parse("menu.xml");
 
         Document document = parser.getDocument();
-        Element root = document.getDocumentElement();
 
+        Element appertizersElement = document.getDocumentElement();
+        NodeList appertizerNodes = appertizersElement.getElementsByTagName(MenuTagName.APPETIZER.toString().toLowerCase());
 
-        NodeList foodNodes = root.getElementsByTagName("food");
-        Food food = null;
+        Appetizer appetizer = null;
 
-        for (int i = 0; i < foodNodes.getLength(); i++) {
-            food = new Food();
-            Element foodElement = (Element) foodNodes.item(i);
-            food.setId(foodElement.getAttribute("id"));
-            food.setName(getSingleChild(foodElement, "name").getTextContent().trim());
-            food.setDescription(getSingleChild(foodElement, "description").getTextContent().trim());
-            menu.add(food);
+        for (int i = 0; i < appertizerNodes.getLength(); i++) {
+            appetizer = new Appetizer();
+            //food = new Food();
+            Element appertizerElement = (Element) appertizerNodes.item(i);
+            appetizer.setName(appertizerElement.getAttribute(MenuAttributeName.NAME.toString().toLowerCase()));
+
+            //  food.setName(getSingleChild(foodElement, "name").getTextContent().trim());
+        //    food.setDescription(getSingleChild(foodElement, "description").getTextContent().trim());
+        //    menu.add(food);
+              appetizersMap.put(appetizer,null);
+              NodeList foodNodes = appertizerElement.getElementsByTagName(MenuTagName.FOOD.toString().toLowerCase());
+              Food food;
+
+            for (int j = 0; j < foodNodes.getLength(); j++ ){
+                food = new Food();
+                Element foodElement = (Element) foodNodes.item(i);
+                food.setId(foodElement.getAttribute(MenuAttributeName.ID.toString().toLowerCase()));
+                food.setName(getSingleChild(foodElement, MenuTagName.NAME.toString().toLowerCase()).getTextContent().trim());
+                food.setPicture(getSingleChild(foodElement, MenuTagName.PICTURE.toString().toLowerCase()).getTextContent().trim());
+                food.setPortion(getSingleChild(foodElement, MenuTagName.PORTION.toString().toLowerCase()).getTextContent().trim());
+            }
         }
-        for (Food f: menu) {
-            System.out.println(f.getName() + ", " + f.getId() + ", " + f.getDescription());
+
+        Iterator<Map.Entry<Appetizer,List<Food>>> iterator = appetizersMap.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Appetizer,List<Food>> pair = iterator.next();
+            System.out.println("ТИПЫ: " + pair.getKey().getName());
         }
+
     }
 
     private static Element getSingleChild(Element element, String childName){
