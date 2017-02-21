5803fa4741c09ad41acf3c6497ccf7ba035f8506
ivan_surus <Ivan@sklbook.mks.local>
2017-02-21 12:37:55 +0300

    refactor

--- src/task5/sax/dom/DOMMenuParser.java
+++ src/task5/sax/dom/DOMMenuParser.java
@@ -10,55 +10,91 @@ import task5.sax.bean.Appetizer;
 import task5.sax.bean.Food;
 import task5.sax.bean.menuName.MenuAttributeName;
 import task5.sax.bean.menuName.MenuTagName;
-
 import java.io.IOException;
 import java.util.*;
 
 
 public class DOMMenuParser {
 
+    public final static String MENU = "menu.xml";
+
     public static void main(String[] args) throws SAXException, IOException{
 
-        Map<Appetizer, List<Food>> appetizersMap = new HashMap<>();
-        List<Food> menu = new ArrayList<Food>();
-        //создание DOM-анализатора (Xerces)
-        DOMParser parser = new DOMParser();
-        parser.parse("menu.xml");
 
+
+
+
+        DOMParser parser = new DOMParser();
+        parser.parse(MENU);
         Document document = parser.getDocument();
+
+        Map<Appetizer, List<Food>> appetizersMap = new HashMap<>();
+        List<Food> foodList;
 
         Element appertizersElement = document.getDocumentElement();
         NodeList appertizerNodes = appertizersElement.getElementsByTagName(MenuTagName.APPETIZER.toString().toLowerCase());
 
-        Appetizer appetizer = null;
 
+        Appetizer appetizer;
         for (int i = 0; i < appertizerNodes.getLength(); i++) {
+            foodList = new ArrayList<>();
             appetizer = new Appetizer();
-            //food = new Food();
             Element appertizerElement = (Element) appertizerNodes.item(i);
             appetizer.setName(appertizerElement.getAttribute(MenuAttributeName.NAME.toString().toLowerCase()));
-
-            //  food.setName(getSingleChild(foodElement, "name").getTextContent().trim());
-        //    food.setDescription(getSingleChild(foodElement, "description").getTextContent().trim());
-        //    menu.add(food);
-              appetizersMap.put(appetizer,null);
-              NodeList foodNodes = appertizerElement.getElementsByTagName(MenuTagName.FOOD.toString().toLowerCase());
-              Food food;
+            appetizersMap.put(appetizer, null);
+            NodeList foodNodes = appertizerElement.getElementsByTagName(MenuTagName.FOOD.toString().toLowerCase());
+            Food food;
 
             for (int j = 0; j < foodNodes.getLength(); j++ ){
                 food = new Food();
-                Element foodElement = (Element) foodNodes.item(i);
+                Element foodElement = (Element) foodNodes.item(j);
                 food.setId(foodElement.getAttribute(MenuAttributeName.ID.toString().toLowerCase()));
                 food.setName(getSingleChild(foodElement, MenuTagName.NAME.toString().toLowerCase()).getTextContent().trim());
                 food.setPicture(getSingleChild(foodElement, MenuTagName.PICTURE.toString().toLowerCase()).getTextContent().trim());
                 food.setPortion(getSingleChild(foodElement, MenuTagName.PORTION.toString().toLowerCase()).getTextContent().trim());
+                foodList.add(food);
+                NodeList typeNodes = foodElement.getElementsByTagName(MenuTagName.TYPE.toString().toLowerCase());
+                for (int k=0; k < typeNodes.getLength(); k++) {
+                    Element typeElement = (Element) typeNodes.item(k);
+                    List<String> type = new ArrayList<>();
+                    type.add(typeElement.getAttribute(MenuAttributeName.ID.toString().toLowerCase()));
+                    type.add(getSingleChild(typeElement, MenuTagName.DESCRIPTION.toString().toLowerCase()).getTextContent().trim());
+                    type.add(getSingleChild(typeElement, MenuTagName.PRICE.toString().toLowerCase()).getTextContent().trim());
+                    food.setTypes(type.get(0),type.get(1),type.get(2));
+                }
             }
+            appetizersMap.put(appetizer,foodList);
         }
+        //вывод
+        Iterator<Map.Entry<Appetizer, List<Food>>> foodIterator = appetizersMap.entrySet().iterator();
 
-        Iterator<Map.Entry<Appetizer,List<Food>>> iterator = appetizersMap.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<Appetizer,List<Food>> pair = iterator.next();
-            System.out.println("ТИПЫ: " + pair.getKey().getName());
+        while (foodIterator.hasNext()){
+
+            Map.Entry<Appetizer, List<Food>> pair = foodIterator.next();
+
+            System.out.println(pair.getKey().getName());
+
+            for (Food food : pair.getValue()) {
+
+                System.out.println("FOOD ID: " + food.getId());
+                System.out.println("FOOD PICTURE: " + food.getPicture());
+                System.out.println("FOOD NAME: " + food.getName());
+                System.out.println("FOOD PORTION: " + food.getPortion());
+                System.out.println("Количество типов " + food.getTypes().size());
+                Iterator<Map.Entry<String, Map<String,String>>> typesIterator = food.getTypes().entrySet().iterator();
+
+                while (typesIterator.hasNext()){
+                    Map.Entry<String, Map<String,String>> typesPair = typesIterator.next();
+                    System.out.println("TYPE ID: " + typesPair.getKey());
+                    Iterator<Map.Entry<String,String>> typeIterator = typesPair.getValue().entrySet().iterator();
+                    while(typeIterator.hasNext()){
+                        Map.Entry<String,String> typePair = typeIterator.next();
+                        System.out.println("TYPE DESCRIPTION: " + typePair.getKey());
+                        System.out.println("TYPE PRICE: " + typePair.getValue());
+                    }
+                }
+            }
+            System.out.println("Dommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
         }
 
     }
---
+++ src/task5/sax/runner/Main.java
@@ -0,0 +1,20 @@
+package task5.sax.runner;
+
+
+import task5.sax.dom.DOMMenuParser;
+
+import java.util.Map;
+
+public class Main {
+
+    private static Map menu;
+
+    public static void main(String[] args) {
+
+
+        DOMMenuParser domParser = new DOMMenuParser();
+        menu = domParser.
+
+    }
+
+}
